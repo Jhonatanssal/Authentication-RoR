aef07b70c14ce3b7ea07b1d8d2bd5948b0e87150
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-10-01 10:10:23 -0500

    adding some helper methods

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -12,4 +12,10 @@ module PostsHelper
       link_to 'Edit', edit_post_path(post), class: 'btn btn-warning text-white mb-2'
     end
   end
+
+  def destroy_button(post)
+    if user_signed_in?
+      link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' } , class: 'btn btn-danger text-white mb-2'
+    end
+  end
 end
