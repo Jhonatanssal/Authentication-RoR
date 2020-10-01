3faba6a87e9b8bc76f3db0873d3adbb8e287f58a
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-09-30 23:52:53 -0500

    add some helpers

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,15 +1,15 @@
 module PostsHelper
-  def username
+  def username(post)
     if user_signed_in?
-      @post.user.username
+      post.user.username
     else
       "by Anonymous"
     end
   end
 
-  def edit_button
+  def edit_button(post)
     if user_signed_in?
-      link_to 'Edit', edit_post_path(@post), class: 'btn btn-warning'
+      link_to 'Edit', edit_post_path(post), class: 'btn btn-warning text-white mb-2'
     end
   end
 end
